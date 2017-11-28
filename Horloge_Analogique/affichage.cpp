5fd79f442d5074c0d73e620e66653a848d9510bb
TurWolf <turwolf@Tenax.home>
2017-11-28 15:15:51 +0100

    MAJ settings dans affichage

--- Horloge_Analogique/affichage.cpp
+++ Horloge_Analogique/affichage.cpp
@@ -10,6 +10,7 @@ Affichage::Affichage(QWidget *parent) :
     ui->setupUi(this);
     connect(ui->buttonBox,SIGNAL(accepted()),this, SLOT(accept()));
     connect(ui->buttonBox,SIGNAL(rejected()),this, SLOT(reject()));
+    connect(this, SIGNAL(accepted()), this, SLOT(confirmeSettings()));
     ui->comboBoxTypeHorloge->addItem("Analogique");
     ui->comboBoxTypeHorloge->addItem("Numerique");
     ui->comboBoxTypeHorloge->setCurrentIndex(m_settings.value("TypeHorloge", 0).toInt());
@@ -23,5 +24,17 @@ Affichage::~Affichage()
 
 void Affichage::setTypeHorloge(int type)
 {
-        m_settings.setValue("TypeHorloge", type);
+    m_typeHorlogeTMP = type;
+}
+
+void Affichage::confirmeSettings()
+{
+    m_settings.setValue("TypeHorloge", m_typeHorlogeTMP);
+    m_settings.setValue("FormeAiguille", m_formeAiguilleTMP);
+    m_settings.setValue("TypeDeFond", m_typeDeFondTMP);
+    m_settings.setValue("CouleurHeure", m_couleurHeureTMP);
+    m_settings.setValue("CouleurMinute", m_couleurMinuteTMP);
+    m_settings.setValue("CouleurSecondes", m_couleurSecondeTMP);
+    m_settings.setValue("CouleurTexte", m_couleurTexteTMP);
+    m_settings.setValue("Transparence", m_transparenceTMP);
 }
--- Horloge_Analogique/affichage.h
+++ Horloge_Analogique/affichage.h
@@ -25,8 +25,17 @@ public:
 private:
     Ui::Affichage *ui;
     QSettings m_settings;
+    int m_typeHorlogeTMP;
+    int m_formeAiguilleTMP;
+    int m_typeDeFondTMP;
+    int m_couleurHeureTMP;
+    int m_couleurMinuteTMP;
+    int m_couleurSecondeTMP;
+    int m_couleurTexteTMP;
+    int m_transparenceTMP;
 private slots:
     void setTypeHorloge(int type);
+    void confirmeSettings();
 };
 
 #endif // AFFICHAGE_H
